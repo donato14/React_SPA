2e2108143f3c91ada9bb1af8191bb1349deb7edc
Kang Kujun <donato14@naver.com>
2022-03-03 14:17:33 +0900

    🚧 Add History function

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,6 +3,7 @@ import { Link, Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 const App = () => {
   return (
@@ -17,6 +18,9 @@ const App = () => {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">history 예제</Link>
+        </li>
         {/* <li>
           <Link to="/profile/gildong">gildong 프로필</Link>
         </li> */}
@@ -26,6 +30,7 @@ const App = () => {
       <Route path={['/about', '/info']} component={About} />
       {/* <Route path="/profile/:username" component={Profile} /> */}
       <Route path='/profiles' component={Profiles} />
+      <Route path='/history' component={HistorySample} />
     </div>
   );
 };
